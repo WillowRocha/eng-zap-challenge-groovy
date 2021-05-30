41d178bf94d22313b74e52bc74e5a55b5264cce7
Willow Rocha <willow.rocha@xproglabs.com>
2021-05-30 12:03:52 -0300

    Initial Commit

---
+++ src/main/groovy/eng/zap/challenge/zap/ZapApplication.groovy
@@ -0,0 +1,13 @@
+package eng.zap.challenge.zap
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ZapApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ZapApplication, args)
+	}
+
+}
---
+++ src/test/groovy/eng/zap/challenge/zap/ZapApplicationTests.groovy
@@ -0,0 +1,13 @@
+package eng.zap.challenge.zap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
